2c1981dbff3935032cf785b6a6c5f86427caf053
Zakarea123 <zakreaalamor12@gmail.com>
2025-10-14 09:08:44 +0200

    Added player health and stamina stats

--- Scripts/Characters/Base/BaseCharacter.cs
+++ Scripts/Characters/Base/BaseCharacter.cs
@@ -10,6 +10,12 @@ public abstract partial class BaseCharacter : CharacterBody2D
 	protected AnimatedSprite2D	animatedSprite;
 	protected float facingDirection = 1;
 	protected CharacterState currentState = CharacterState.Idle;
+	protected float stamina;
+	protected float maxStamina;
+	protected float staminaRestored;
+	protected float maxHealth; 
+	protected float health;
+	
 	
 	
 	public override void _Ready()
@@ -23,15 +29,16 @@ public abstract partial class BaseCharacter : CharacterBody2D
 		HandleInput();
 		HandleMovement();
 		HandleAnimation();
+		stamina = Mathf.Min(maxStamina, stamina + staminaRestored * (float)delta);
 	}
 
 	protected virtual void HandleInput()
 	{
-		if (Input.IsActionPressed("AttackNormal"))
+		if (Input.IsActionPressed("AttackNormal") && CanAttack(10f))
 		{
 			currentState = CharacterState.AttackNormal;
 		}
-		else if (Input.IsActionPressed("AttackHeavy"))
+		else if (Input.IsActionPressed("AttackHeavy") && CanAttack(20f))
 		{
 			currentState = CharacterState.AttackHeavy;
 		}
@@ -39,6 +46,12 @@ public abstract partial class BaseCharacter : CharacterBody2D
 		{
 			currentState = CharacterState.SpecialAbility;
 		}
+		//Temp 
+		else if (Input.IsActionJustReleased("TakeDamage"))
+		{
+			TakeDamage(20f);
+		}
+		//Temp
 		else
 		{
 			Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
@@ -98,7 +111,47 @@ public abstract partial class BaseCharacter : CharacterBody2D
 			currentState = direction != Vector2.Zero ? CharacterState.Moving : CharacterState.Idle;
 		}
 	}
-	
 	protected abstract void UseAbility();
+	
+	
+	// -------------- Stamina System -----------------
+	protected bool SpendStamina(float amount)
+	{
+		if (stamina >= amount)
+		{
+			stamina -= amount;
+			return true;
+		}
+		return false;
+	}
+	
+	protected virtual bool CanAttack(float cost)
+	{
+		return true;
+	}
+	
+	// ------------------- Health System ------------------
+
+	protected virtual void TakeDamage(float amount)
+	{
+		health -= amount;
+		health = Mathf.Max(health, 0);
+		UpdateHealthBar();
+
+		if (health <= 0)
+		{
+			Console.WriteLine("You Died");
+			//Die();
+		}
+	}
+
+	// Add animations later to make the player die
+	protected virtual void Die()
+	{
+		QueueFree();
+	}
+
+	// Handled by the Character class
+	protected virtual void UpdateHealthBar(){ }
 	
 }
--- Scripts/Characters/Players/PlayerWarrior.cs
+++ Scripts/Characters/Players/PlayerWarrior.cs
@@ -8,6 +8,37 @@ namespace Characters.Players.PlayerWarrior;
 public partial class PlayerWarrior : BaseCharacter
 {
     protected bool isGaurd;
+    private const float normalAttackCost = 10f;
+    private const float hevayAttackCost = 20f;
+    private ProgressBar staminaBar;
+    private ProgressBar healthBar;
+
+    public override void _Ready()
+    {
+        // Stamina initialize 
+        base._Ready();
+        maxStamina = 100f;
+        stamina = maxStamina;
+        staminaRestored = 20f;
+        staminaBar = GetNode<ProgressBar>("UI/StaminaBar");
+        
+        // Health initialize
+        maxHealth = 100f;
+        health = maxHealth;
+        healthBar = GetNode<ProgressBar>("UI/HealthBar");
+        healthBar.MaxValue = maxHealth;
+        healthBar.Value = health;
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        staminaBar.Value = stamina;
+    }
+    
+    
+    
     protected override void UseAbility()
     {
         isGaurd = true;
@@ -24,5 +55,42 @@ public partial class PlayerWarrior : BaseCharacter
             currentState = direction != Vector2.Zero ? CharacterState.Moving : CharacterState.Idle;
         };
         timer.Start();
+    }
+
+    protected override void HandleInput()
+    {
+        base.HandleInput();
+
+        if (Input.IsActionJustPressed("AttackNormal"))
+        {
+            if (stamina >= normalAttackCost)
+            {
+                SpendStamina(normalAttackCost); 
+                currentState = CharacterState.AttackNormal;
+            }
+        }
+        else if (Input.IsActionJustPressed("AttackHeavy"))
+        {
+            if (stamina >= hevayAttackCost)
+            {
+                SpendStamina(hevayAttackCost);
+                currentState = CharacterState.AttackHeavy;
+            }
+                
+        }
+    }
+    
+    protected override bool CanAttack(float cost)
+    {
+        return stamina >= cost;
+    }
+
+    protected override void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.Value = health;
+        }
+        
     }
 }
